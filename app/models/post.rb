251ff649425c1df8c66fabae5f303e9c8e503078
Yonezawa Takuto <y1tkt.dev@gmail.com>
2020-10-03 22:28:47 +0900

    Postモデルへアノテーションの追記

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,11 @@
+# == Schema Information
+#
+# Table name: posts
+#
+#  id         :integer          not null, primary key
+#  content    :text(65535)
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
 class Post < ApplicationRecord
 end
--- test/models/post_test.rb
+++ test/models/post_test.rb
@@ -1,3 +1,12 @@
+# == Schema Information
+#
+# Table name: posts
+#
+#  id         :integer          not null, primary key
+#  content    :text(65535)
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
 require 'test_helper'
 
 class PostTest < ActiveSupport::TestCase
