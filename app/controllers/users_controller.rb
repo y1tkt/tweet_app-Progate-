378079a4859b330e211782ea6fdfc0c141bb54c0
Yonezawa Takuto <y1tkt.dev@gmail.com>
2020-10-06 00:39:09 +0900

    新規登録時の初期アイコンをランダム化

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -12,10 +12,28 @@ class UsersController < ApplicationController
   end
 
   def create
+    case rand(1..8)
+    when 1
+      image_name = "default_user_BLU.png"
+    when 2
+      image_name = "default_user_CYN.png"
+    when 3
+      image_name = "default_user_GRN.png"
+    when 4
+      image_name = "default_user_GRY.png"
+    when 5
+      image_name = "default_user_PNK.png"
+    when 6
+      image_name = "default_user_PPL.png"
+    when 7
+      image_name = "default_user_RED.png"
+    when 8
+      image_name = "default_user_YEL.png"
+    end
     @user = User.new(
       name: params[:name],
       email: params[:email],
-      image_name: "default_user_GRY.png"
+      image_name: image_name
     )
     if @user.save
       flash[:notice] = "ユーザー登録が完了しました"
