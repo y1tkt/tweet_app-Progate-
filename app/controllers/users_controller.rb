3d69d2ba6ef4ce932ee783d8916ae1a3d7fee00b
Yonezawa Takuto <y1tkt.dev@gmail.com>
2020-10-05 21:05:59 +0900

    新規登録時のバリデーションチェックの実装、エラーメッセージの表示、flashによるサクセスメッセージの表示

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,11 +8,16 @@ class UsersController < ApplicationController
   end
 
   def new
+    @user = User.new
   end
 
   def create
     @user = User.new(name: params[:name], email: params[:email])
-    @user.save
-    redirect_to("/users/#{@user.id}")
+    if @user.save
+      flash[:notice] = "ユーザー登録が完了しました"
+      redirect_to("/users/#{@user.id}")
+    else
+      render("users/new")
+    end
   end
 end
