8ff4850d7abe13b3ab9df84d181d57aa0e80ca89
Yonezawa Takuto <y1tkt.dev@gmail.com>
2020-10-05 20:56:13 +0900

    新規登録機能の実装

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,4 +9,10 @@ class UsersController < ApplicationController
 
   def new
   end
+
+  def create
+    @user = User.new(name: params[:name], email: params[:email])
+    @user.save
+    redirect_to("/users/#{@user.id}")
+  end
 end
