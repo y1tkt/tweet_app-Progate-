49ff792eada45fcbca0b2e4029452b537d9e6224
Yonezawa Takuto <y1tkt.dev@gmail.com>
2020-10-05 20:44:38 +0900

    新規登録ページ（users#new）の作成

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,4 +6,7 @@ class UsersController < ApplicationController
   def show
     @user = User.find_by(id: params[:id])
   end
+
+  def new
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  get 'signup' => 'users#new'
   get 'users/index' => 'users#index'
   get 'users/:id' => 'users#show'
 
