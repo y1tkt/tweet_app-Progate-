bd8624203f575a9376735f2095f1ba74a5f3f509
Yonezawa Takuto <y1tkt.dev@gmail.com>
2020-10-07 20:39:46 +0900

    Usersテーブルから該当するユーザ情報の取得

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -68,4 +68,14 @@ class UsersController < ApplicationController
 
   def login_form
   end
+
+  def login
+    @user = User.find_by(email: params[:email], password: params[:password])
+    if @user
+      flash[:notice] = "ログインしました"
+      redirect_to "/posts/index"
+    else
+      render("users/login_form")
+    end
+  end
 end
