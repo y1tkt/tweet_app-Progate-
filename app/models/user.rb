ad22662cd98bcd230e488451a776ba39a90fe48e
Yonezawa Takuto <y1tkt.dev@gmail.com>
2020-10-07 13:29:33 +0900

    Usersテーブルにpasswordカラムの追加、Userモデルへのバリデーション設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,10 +6,12 @@
 #  email      :string(255)
 #  image_name :string(255)
 #  name       :string(255)
+#  password   :string(255)
 #  created_at :datetime         not null
 #  updated_at :datetime         not null
 #
 class User < ApplicationRecord
   validates :name, presence: true
   validates :email, presence: true, uniqueness: true
+  validates :password, presence: true
 end
