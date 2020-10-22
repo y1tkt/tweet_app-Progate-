0824336546fe2a7df4af7c6ca667eba195509fd9
Yonezawa Takuto <y1tkt.dev@gmail.com>
2020-10-22 22:05:19 +0900

    いいね済み投稿一覧ページ（users#likes）の作成

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,7 @@ Rails.application.routes.draw do
   get 'signup' => 'users#new'
   get 'users/index' => 'users#index'
   get 'users/:id' => 'users#show'
+  get "users/:id/likes" => "users#likes"
 
   get 'posts/index' => 'posts#index'
   get 'posts/new' => 'posts#new'
