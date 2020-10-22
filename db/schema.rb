611ebaca54616f28bd82a1d4984cff777735bed2
Yonezawa Takuto <y1tkt.dev@gmail.com>
2020-10-22 09:23:02 +0900

    Likeモデル、Likesテーブルの生成、バリデーション設定

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20201010082429) do
+ActiveRecord::Schema.define(version: 20201022002001) do
+
+  create_table "likes", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+    t.integer  "user_id"
+    t.integer  "post_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "posts", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.text     "content",    limit: 65535
