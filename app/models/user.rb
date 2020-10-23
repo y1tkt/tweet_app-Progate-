308f58a39630468136097891134519372d7ac0eb
Yonezawa Takuto <y1tkt.dev@gmail.com>
2020-10-23 13:17:12 +0900

    gem 'bcrypt'のインストール、has_secure_passwordの設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,6 +11,8 @@
 #  updated_at :datetime         not null
 #
 class User < ApplicationRecord
+  has_secure_password
+
   validates :name, presence: true
   validates :email, presence: true, uniqueness: true
   validates :password, presence: true
