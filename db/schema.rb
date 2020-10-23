0bcfca6010fb31baf5a45d2b759d14604a82b561
Yonezawa Takuto <y1tkt.dev@gmail.com>
2020-10-23 13:18:08 +0900

    Usersテーブルにpassword_digestカラムを追加、passwordカラムを削除

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20201022002001) do
+ActiveRecord::Schema.define(version: 20201023041203) do
 
   create_table "likes", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.integer  "user_id"
@@ -29,10 +29,10 @@ ActiveRecord::Schema.define(version: 20201022002001) do
   create_table "users", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string   "name"
     t.string   "email"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
     t.string   "image_name"
-    t.string   "password"
+    t.string   "password_digest"
   end
 
 end
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -2,13 +2,13 @@
 #
 # Table name: users
 #
-#  id         :integer          not null, primary key
-#  email      :string(255)
-#  image_name :string(255)
-#  name       :string(255)
-#  password   :string(255)
-#  created_at :datetime         not null
-#  updated_at :datetime         not null
+#  id              :integer          not null, primary key
+#  email           :string(255)
+#  image_name      :string(255)
+#  name            :string(255)
+#  password_digest :string(255)
+#  created_at      :datetime         not null
+#  updated_at      :datetime         not null
 #
 require 'test_helper'
 
