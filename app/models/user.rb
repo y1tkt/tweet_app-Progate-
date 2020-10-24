1392271bfbce1550b6c6912866c785dd90aa5987
Yonezawa Takuto <y1tkt.dev@gmail.com>
2020-10-24 13:00:04 +0900

    Userモデルにバリデーションの追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -14,9 +14,22 @@
 class User < ApplicationRecord
   has_secure_password
 
-  validates :name, presence: true
-  validates :email, presence: true, uniqueness: true
-  validates :account_id, presence: true, uniqueness: true
+  validates :name,
+    presence: true,
+    length: { maximum: 20 }
+  validates :email,
+    presence: true,
+    uniqueness: true
+  validates :account_id,
+    presence: true,
+    uniqueness: true,
+    length: { maximum: 16 },
+    format: {
+      with: /\A@\w+\z/,
+      message: 'は英数字で入力してください。使える記号はアンダースコア(_)のみです。'
+    }
+  validates :password,
+    length: { minimum: 8 }
 
   def posts
     return Post.where(user_id: self.id)
