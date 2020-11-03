03749ca67b97ed266f24a36ae4fd48135737a5ea
Yonezawa Takuto <y1tkt.dev@gmail.com>
2020-11-03 16:19:36 +0900

    /users/new：form_tagをform_forに書き換え
    ・新規登録ページ（users#new）のフォームをform_tagからform_forに書き換え
    ・ストロングパラメータを使用するように改修

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -34,14 +34,8 @@ class UsersController < ApplicationController
     when 8
       image_name = "default_user_YEL.png"
     end
-    @user = User.new(
-      name: params[:name],
-      account_id: params[:account_id],
-      email: params[:email],
-      password: params[:password],
-      password_confirmation: params[:password_confirmation],
-      image_name: image_name
-    )
+    @user = User.new(user_params)
+    @user.image_name = image_name
     if @user.save
       session[:user_id] = @user.id
       flash[:notice] = "ユーザー登録が完了しました"
@@ -107,5 +101,11 @@ class UsersController < ApplicationController
       flash[:notice]  = "権限がありません"
       redirect_to "/posts/index"
     end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :account_id, :email, :password, :password_confirmation)
   end
 end
